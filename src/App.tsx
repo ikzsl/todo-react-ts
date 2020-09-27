539b8c7d882852ca45bb811150986b5b0901d7e2
ikzsl <ikzsl@yandex.ru>
2020-09-28 01:23:29 +0300

    add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Navbar} from './components/Navbar'
+import {TodoForm} from "./components/TodoForm";
+import {TodoList} from "./components/TodoList";
+import {ITodo} from "./interfaces";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    const addHandler = (title: string) => {
+      const newTodo: ITodo = {title: title, id: Date.now(), completed: false};
+      setTodos((prevState) => [newTodo, ...prevState])
+    }
+
+    const toggleHandler = (id: number) => {
+      setTodos(prev =>
+        prev.map(todo => {
+          if (todo.id === id) {
+            console.log(111111111111, todo.completed, todo.id);
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        }))
+    }
+
+    const removeHandler = (id: number) => {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+
+    return (
+      <>
+        <Navbar>qqq</Navbar>
+        <div className="container">
+          <TodoForm
+            onAdd={addHandler}/>
+          <TodoList
+            todos={todos}
+            onToggle={toggleHandler}
+            onRemove={removeHandler}/>
+        </div>
+      </>
+
+    )
+  }
+;
+
 
 export default App;
